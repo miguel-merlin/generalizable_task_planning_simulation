0fa4b437fabe4ff4ac5f55aac1949bb93b5c7a34
miguel-merlin <mmerlin@stevens.edu>
2024-03-06 19:26:47 -0500

    feat: Added cube to main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from simulation.environment import setup_environment
-from simulation.objects import load_table
+from simulation.objects import load_table, load_cube
 import pybullet as p
 import time
 
 def main():
     physics_client_id = setup_environment()
 
-    # Load objects to simulation
+    # Load table
     load_table(physics_client_id)
+    
+    # Load cube
+    load_cube(physics_client_id)
 
     # Simulation Loop
     for i in range(10000):
