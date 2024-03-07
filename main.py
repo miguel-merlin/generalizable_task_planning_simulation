2e89427028f5170efa1fc177e83d2c4038b35368
miguel-merlin <mmerlin@stevens.edu>
2024-03-06 19:33:26 -0500

    feat: Added multiple cubes to simulation

--- main.py
+++ main.py
@@ -3,14 +3,34 @@ from simulation.objects import load_table, load_cube
 import pybullet as p
 import time
 
+
+TABLE_HEIGHT = 1
+CUBE_SIZE = 0.2
+
 def main():
     physics_client_id = setup_environment()
 
     # Load table
     load_table(physics_client_id)
     
-    # Load cube
-    load_cube(physics_client_id)
+    # Load cubes on top of the table at different positions
+    load_cube(
+            physics_client_id,
+            base_position = [0.2, 0, TABLE_HEIGHT + CUBE_SIZE/2],
+            cube_size = CUBE_SIZE
+    )
+
+    load_cube(
+        physics_client_id,
+        base_position = [0, 0.2, TABLE_HEIGHT + CUBE_SIZE/2],
+        cube_size = CUBE_SIZE
+    )
+
+    load_cube(
+        physics_client_id,
+        base_position = [-0.2, -0.2, TABLE_HEIGHT + CUBE_SIZE/2],
+        cube_size = CUBE_SIZE
+    )
 
     # Simulation Loop
     for i in range(10000):
